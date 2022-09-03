cfad53c79776d244facfcd2ad59439c39de9c735
toncrad <106417165+toncrad@users.noreply.github.com>
2022-09-02 21:38:13 -0700

    Add files via upload

---
+++ VIDEO PREVIEW/script.js
@@ -0,0 +1,14 @@
+console.log("page loaded...");
+
+var x = document.getElementById("film");
+
+function over(div) {
+    var x = document.getElementById("film");
+    x.play()
+}
+
+
+function out(div) {
+    var x = document.getElementById("film");
+    x.pause()
+}
