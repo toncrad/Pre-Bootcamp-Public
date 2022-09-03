97609f5c102ace19cbdb8919c448800ac563412b
toncrad <106417165+toncrad@users.noreply.github.com>
2022-09-02 21:59:18 -0700

    Add files via upload

---
+++ LIKE/script2.js
@@ -0,0 +1,32 @@
+var count = 9;
+var countElement = document.querySelector("#count");
+
+console.log(countElement);
+
+function add1() {
+    count++;
+    countElement.innerText = count+" like(s)";
+    console.log(count);
+}
+
+var count_2 = 12;
+var countElement_2 = document.querySelector("#count_2");
+
+console.log(countElement);
+
+function add1_2() {
+    count_2++;
+    countElement_2.innerText = count_2+" like(s)";
+    console.log(count);
+}
+
+var count_3 = 9;
+var countElement_3 = document.querySelector("#count_3");
+
+console.log(countElement);
+
+function add1_3() {
+    count_3++;
+    countElement_3.innerText = count_3+" like(s)";
+    console.log(count);
+}
