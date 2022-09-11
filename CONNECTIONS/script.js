4f5088ebbdc4754a44cecbffeacbd4d1f5a4b7d1
toncrad <106417165+toncrad@users.noreply.github.com>
2022-09-11 16:12:00 -0700

    Add files via upload

---
+++ CONNECTIONS/script.js
@@ -0,0 +1,9 @@
+function hide2() {
+    document.querySelector(".Todd").remove();
+}
+function hide() {
+    document.querySelector(".Phil").remove();
+}
+function change_text(){
+    document.getElementById("username").innerHTML = "Anthony Craddock";
+}
