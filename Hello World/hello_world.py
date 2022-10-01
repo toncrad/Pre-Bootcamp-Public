160724ca139894bebaec0d87072db01e3a9e15e8
toncrad <106417165+toncrad@users.noreply.github.com>
2022-09-30 17:30:08 -0700

    Add files via upload

---
+++ Hello World/hello_world.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+name = "Anthony"
+print("Hello", name)
+print("Hello" + name)
+
+number = "14"
+print("Hello", number)
+print("Hello" + number)
+
+fave_food1 = "raspberries"
+fave_food2 = "clams"
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+
+fav_food1 = "artichokes"
+fav_food2 = "oysters"
+print(f"I love to eat {fav_food1} and {fav_food2}.")
